349cbca62ce47d4f443d5371309476a4b21662bb
Jingbo Wu <jw2531@columbia.edu>
2026-08-26 12:04:47 -0700

    fix the typoed institution id in esacci-lst-source-ids

--- src/institution_ids.py
+++ src/institution_ids.py
@@ -56,3 +56,4 @@ institution_id['institution_id']['WEGC'] = 'Wegener Center for Climate and Globa
 institution_id['institution_id']['WUR'] = 'Wageningen Universit, Netherlands'
 institution_id['institution_id']['KNMI-WUR'] = 'Multi-institute/agency collaboration,KNMI-WUR'
 institution_id['institution_id']['ULeicester'] = 'University of Leicester, Leicester, UK'
+institution_id['institution_id']['Estellus'] = 'Estellus, Fr'
